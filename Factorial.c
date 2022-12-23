8a45b3d6e0c90dd698d8112ba58327581442b311
Piyush Narayan <piyushvaish51655@gmail.com>
2022-12-23 23:12:39 +0530

    Factorial using Recursion

--- Factorial.c
+++ Factorial.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int factorial(int num)
+int factorial(int num)        // user defined function
 {
     if (num==0 || num==1)
     {
@@ -7,15 +7,15 @@ int factorial(int num)
     }
     else
     {
-        return num*factorial(num-1);
+        return num*factorial(num-1);    // Recursive call
     }    
 }
 
 int main()
 {
     int num;
-    printf("Enter a number\n");
+    printf("Enter a number\n");      // Input from the user
     scanf("%d", &num);
-    printf("The factorial of %d is %d", num, factorial(num));   
+    printf("The factorial of %d is %d\n", num, factorial(num));      // Printing answer
     return 0;
 }
