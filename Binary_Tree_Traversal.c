f336b733673ddcfcae1095981ef825cd018f0fed
piyush9012 <121113213+piyush9012@users.noreply.github.com>
2023-03-06 19:08:35 +0530

    Update Binary_Tree_Traversal.c

--- Binary_Tree_Traversal.c
+++ Binary_Tree_Traversal.c
@@ -14,7 +14,7 @@ struct node *getnode(int val)
 	newnode = malloc(sizeof(struct node));  
 	newnode->key = val;  
 	newnode->left = NULL;
-    	newnode->right = NULL;  
+    newnode->right = NULL;  
    	return newnode;
 } 
 	
@@ -25,7 +25,7 @@ struct node *insertnode(struct node *root, int val)
 	if(root->key < val)  
 		root->right = insertnode(root->right,val);  
 	if(root->key > val)  
-    		root->left = insertnode(root->left,val);  
+    	root->left = insertnode(root->left,val);  
 	return root;
 }
 	
@@ -33,15 +33,15 @@ void inorder(struct node *root)
 {
 	if(root == NULL)  
 	    return;    
-    	inorder(root->left);  	        //traverse the left subtree 
-    	printf("%d  ",root->key);  	//visit the root   
-    	inorder(root->right);           //traverse the right subtree
+    inorder(root->left);  	        //traverse the left subtree 
+	printf("%d  ",root->key);  	    //visit the root   
+	inorder(root->right);           //traverse the right subtree
 }
 
 void preorder(struct node *root) 
 {  
-    	if(root == NULL)  
-        	return; 
+    if(root == NULL)  
+    	return; 
 	printf("%d  ",root->key); 
 	preorder(root->left);  
 	preorder(root->right); 
@@ -52,8 +52,8 @@ void postorder(struct node *root)
 	if(root == NULL)  
 		return;  
 	postorder(root->left); 
-    	postorder(root->right);
-    	printf("%d  ",root->key);
+    postorder(root->right);
+	printf("%d  ",root->key);
 }  
 
 int main()  
@@ -61,37 +61,37 @@ int main()
 	int choice;
 	struct node *root = NULL;  
 	while(1)      
+    {  
+    	printf("\nSelect the operation\n");
+    	printf("---------------------\n"); 
+    	printf("1. Insert new node in Binary Tree\n2. Display nodes of Binary Tree(via Inorder Traversal)\n3. Display nodes of Binary Tree(via Preorder Traversal)\n4. Display nodes of Binary Tree(via Postorder Traversal)\n5. Exit\n");  
+    	scanf("%d", &choice);              
+    	switch(choice)  
     	{  
-        	printf("\nSelect the operation\n");
-        	printf("---------------------\n"); 
-        	printf("1. Insert new node in Binary Tree\n2. Display nodes of Binary Tree(via Inorder Traversal)\n3. Display nodes of Binary Tree(via Preorder Traversal)\n4. Display nodes of Binary Tree(via Postorder Traversal)\n5. Exit\n");  
-	    	scanf("%d", &choice);              
-	    	switch(choice)  
-	    	{  
-	        	case 1:   
-	            		int data;
+        	case 1:   
+        		int data;
 				printf("Enter the value to be inserted\n");  
-	        		scanf("%d",&data);  
-	            		root = insertnode(root,data);                    
-	            		break;                            
-            		case 2:
-			    	printf ("Inorder Traversal of the Binary Tree:\n");  
-	            		inorder(root);  
-	            		break;
-            		case 3:
-			    	printf ("Preorder Traversal of the Binary Tree:\n");  
-	            		preorder(root);  
-	            		break;
-            		case 4:
-			    	printf ("Postorder Traversal of the Binary Tree:\n");  
-	            		postorder(root);  
-	            		break;
-            		case 5:
-                		exit(0);
-	        	default:
-	            		printf("Wrong Entry\n");
-                		break;
-        	}                          
-    	}  
+        		scanf("%d",&data);  
+        		root = insertnode(root,data);                    
+        		break;                            
+       		case 2:
+		    	printf ("Inorder Traversal of the Binary Tree:\n");  
+	        	inorder(root);  
+	         	break;
+        	case 3:
+		    	printf ("Preorder Traversal of the Binary Tree:\n");  
+        		preorder(root);  
+          		break;
+       		case 4:
+			    printf ("Postorder Traversal of the Binary Tree:\n");  
+	    		postorder(root);  
+        		break;
+            case 5:
+        		exit(0);
+        	default:
+	            printf("Wrong Entry\n");
+        		break;
+    	}                          
+	}  
  	return 0;  
 }
